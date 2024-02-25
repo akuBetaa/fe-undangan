703ef3d68a9294694ce7859dba9b4b324327dbd7
Beta Nurul Awwalin <beta.n.awwalin@gmail.com>
2024-02-25 23:02:48 +0700

    upload invitation website frontend code

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        'poppins': ['Poppins', 'sans-serif'],
+        'jura': ['Jura', 'sans-serif'],
+        'elsie': ['Elsie', 'cursive'],
+        'playfair': ['Playfair Display', 'serif'],
+      },
+      colors: {
+        'primary' : '#3B4D65',
+      },
+    },
+  },
+  plugins: [],
+}
